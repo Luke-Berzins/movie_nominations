c2f5c3e2e4f66bc04b7a3c4a39bdc7a8e5aec466
Luke-Berzins <lukeberzins16@gmail.com>
2021-05-08 10:06:52 -0400

    form submission under handleSubmit implemented, displaying a submitting notification through short circuiting

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react'
 
 
-
-
 export default function Search(props) {
+    const [submitting, setSubmitting] = useState(false)
     const [state, setState] = useState("hi")
+    
+    const handleSubmit = e => {
+        e.preventDefault()
+        setSubmitting(true)
+        setTimeout(() => {
+            setSubmitting(false)
+        }, 2000)
+    }
     
     
     
     return (
         <div>
-            <form>
+            <div>
+            {submitting && <div>...Submitting!</div>}
+            <p>{state}</p>
+            </div>
+            <form onSubmit={handleSubmit}>
                 <input></input>
                 <button>Search!</button>
             </form>
