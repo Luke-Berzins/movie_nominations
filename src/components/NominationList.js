ad4cf555a85a786834f07a9e5d93af3b02234ccb
Luke-Berzins <lukeberzins16@gmail.com>
2021-05-08 21:48:36 -0400

    adding key index to each nomination list item

--- src/components/NominationList.js
+++ src/components/NominationList.js
@@ -13,7 +13,6 @@ export default function NominationList(props) {
                 title={nominatedMovie}
                 year={props.nominationList[nominatedMovie]}
                 setNominationList={props.setNominationList}
-                nominationList={props.nominationList}
             />
         })
     )
